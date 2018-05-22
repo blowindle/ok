b5e52c0265000790e9c7c85a65bed8d8df8fbc80
blowindle <39296314+blowindle@users.noreply.github.com>
2018-05-22 22:05:30 +0800

    update

--- angular_ratio_3.m
+++ angular_ratio_3.m
@@ -7,7 +7,8 @@ eq2=A*k_0^2-B*(k_0-100)^2;
 S=solve(eq1,eq2);
 S_a=double(S.A);
 S_b=double(S.B);
-i_m1=S_a.*k_1.^3+10.*ones(1,(k_0*100));i_m2=S_b.*(k_2-100.*ones(1,(10001-100*k_0))).^3+24.*ones(1,(10001-100*k_0));
+i_m1=S_a.*k_1.^3+10.*ones(1,(k_0*100));
+i_m2=S_b.*(k_2-100.*ones(1,(10001-100*k_0))).^3+24.*ones(1,(10001-100*k_0));
 i_m=[i_m1,i_m2,24.*ones(1,2000)];g=(i_m-i).^2;xi=1-trapz(k,g);
 if xi>=max
     max=xi;
